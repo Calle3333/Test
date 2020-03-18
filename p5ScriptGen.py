806cc8f416b71b62e818333aa84039f7eff5b5c8
Calle3333 <43997155+Calle3333@users.noreply.github.com>
2020-03-18 18:06:44 +0100

    Add files via upload

---
+++ p5ScriptGen.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from time import sleep
+from shutil import copyfile
+
+os.chdir("..")
+os.chdir("..")
+os.chdir("..")
+os.chdir("Users\CarlJ\Desktop\p5Scripts")
+
+
+#if in p5Scripts
+#path=os.getcwd()
+
+pathOrg=os.path.join(os.getcwd(),"ScriptsFiles")
+pathOrg=os.path.join(pathOrg,"p5.js sketch prefab")
+pathOrgE=os.path.join(pathOrg,"libraries")
+
+if not os.path.exists(pathOrg):
+    print("Error: Missing files")
+    print(pathOrg)
+    print(os.getcwd())
+    sleep(5)
+    sys.exit()
+
+projectName = input("Name: ")
+
+os.makedirs(projectName)
+os.chdir(projectName)
+
+
+
+copyfile(os.path.join(pathOrg,"index.html"),"index.html")
+copyfile(os.path.join(pathOrg,"sketch.js"),"sketch.js")
+
+os.makedirs("libraries")
+os.chdir("libraries")
+
+copyfile(os.path.join(pathOrgE,"p5.dom.js"),"p5.dom.js")
+copyfile(os.path.join(pathOrgE,"p5.js"),"p5.js")
+copyfile(os.path.join(pathOrgE,"p5.sound.js"),"p5.sound.js")
